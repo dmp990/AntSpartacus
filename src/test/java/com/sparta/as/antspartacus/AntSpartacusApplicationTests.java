05d3cb33ff2a9e905aed5c8c6bc42c5994327a45
Asad Ali <asadali.asif.1@gmail.com>
2023-05-24 14:07:22 +0100

    Create project structure

---
+++ src/test/java/com/sparta/as/antspartacus/AntSpartacusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.as.antspartacus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntSpartacusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
